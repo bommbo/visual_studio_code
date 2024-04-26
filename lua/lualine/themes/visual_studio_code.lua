ce13ce2f12a07fb3366f23c9e460c905aa5a9f66
bommbo <miaohaiyang1024@outlook.com>
2024-04-26 13:13:59 +0800

    change color

--- lua/lualine/themes/visual_studio_code.lua
+++ lua/lualine/themes/visual_studio_code.lua
@@ -1,24 +1,24 @@
 local colors = {}
 
 colors.foreground = "#FFFFFF"
-colors.background = "#007ACC"
-colors.header_background = "#2C896B"
+colors.background = "#000000"
+colors.header_background = "#007ACC"
 
 local component_colors = {
-    a = { fg = colors.foreground, bg = colors.header_background },
-    b = { fg = colors.foreground, bg = colors.background },
-    c = { fg = colors.foreground, bg = colors.background },
+	a = { fg = colors.foreground, bg = colors.header_background },
+	b = { fg = colors.foreground, bg = colors.background },
+	c = { fg = colors.foreground, bg = colors.background },
 
-    x = { fg = colors.foreground, bg = colors.background },
-    y = { fg = colors.foreground, bg = colors.background },
-    z = { fg = colors.foreground, bg = colors.background },
+	x = { fg = colors.foreground, bg = colors.background },
+	y = { fg = colors.foreground, bg = colors.background },
+	z = { fg = colors.foreground, bg = colors.background },
 }
 
 return {
-    normal = component_colors,
-    insert = component_colors,
-    visual = component_colors,
-    replace = component_colors,
-    command = component_colors,
-    inactive = component_colors,
+	normal = component_colors,
+	insert = component_colors,
+	visual = component_colors,
+	replace = component_colors,
+	command = component_colors,
+	inactive = component_colors,
 }
